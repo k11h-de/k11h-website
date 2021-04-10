7ed95d97b7cb2c106c5ca92db298f4abe9577299
k11h <mail@k11h.de>
2021-04-10 11:44:30 +0200

    adding path

--- publish-on-change.sh
+++ publish-on-change.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PATH=/usr/bin:/opt/rh/devtoolset-9/root/usr/bin:~/.local/bin
 
 # assuming this script is only run from the folder it is stored in
 # if this is not the case uncomment and adapt next line
